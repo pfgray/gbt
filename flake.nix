0bdebe4a2b0f9bf21390b0a3b6b73cbe45601336
Paul Gray <paul.gray@instructure.com>
2024-03-03 17:19:54 -0500

    Upgrading nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Graph Build Tool";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
